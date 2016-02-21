4844c20e3f76acf7179eeedd5ed95ec786cc07b1
oitsjustjose <stovallj1995@gmail.com>
2016-02-21 10:53:12 -0500

    -

--- Student Registration/src/com/github/fantastic_five/StudentRegistrationMain.java
+++ Student Registration/src/com/github/fantastic_five/StudentRegistrationMain.java
@@ -6,29 +6,37 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+
+import net.miginfocom.swing.MigLayout;
 
 /**
  * 
  * @author Fantastic Five
+ * 
+ *         KNOW YOUR AUDIENCE, KNOW THE TASKS
  *
  */
 
 public class StudentRegistrationMain implements ActionListener
 {
-	// Local variable for our icon, which I will make later
-	private static final String APPLICATION_ICON = "application_icon";
 	// Strings used for command listeners
 	private static final String ACCT_CREATE_ACTION = "account_create";
-	private static final String STUDENT_LOGIN_ACTION = "student_login";
-	private static final String TEACHER_LOGIN_ACTION = "teacher_login";
-	private static final String ADMIN_LOGIN_ACTION = "admin_login";
+	private static final String LOGIN_ACTION = "login_action";
+	private static final String LOGIN_WINDOW_ACTION = "login_window_action";
+	private static final String VIEW_CLASSES_ACTION = "view_classes_action";
+	private static final String CONFIRM_ACCT_CREATION_ACTION = "confirm_account_creation_action";
+
+	private static String currentWIPPassword;
+	private static final ArrayList<String> passwords = new ArrayList<String>();
 
 	// TODO: write everything!
 	public static void main(String[] args)
@@ -38,34 +46,66 @@ public class StudentRegistrationMain implements ActionListener
 			@Override
 			public void run()
 			{
+				// Put anything that needs to run in main here!
 				createMainWindow();
 			}
 		};
 		SwingUtilities.invokeLater(r);
 	}
 
+	/**
+	 * This method creates the first window that is visible to the user
+	 */
+
 	private static void createMainWindow()
 	{
-		// Creates a window with the title "Student Registration"
 		JFrame frame = new JFrame("Student Registration");
-		// Sets it to terminate properly when closing the window
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		// Buttons for all actions
+		// Sets the window to a fixed size
+		frame.setMinimumSize(new Dimension(256, 128));
+		frame.setMaximumSize(new Dimension(256, 128));
+
+		// Create GUI and buttons
+		JPanel gui = new JPanel(new GridLayout());
+		JButton login = new JButton("Button 1");
+		JButton view = new JButton("Button 2");
+
+		gui.add(login);
+		gui.add(view);
+
+		login.setActionCommand(LOGIN_WINDOW_ACTION);
+		login.addActionListener(new StudentRegistrationMain());
+
+		view.setActionCommand(VIEW_CLASSES_ACTION);
+		view.addActionListener(new StudentRegistrationMain());
+
+		frame.getContentPane().add(gui, BorderLayout.CENTER);
+		frame.getContentPane().removeAll();
+		frame.getContentPane().add(gui, BorderLayout.CENTER);
+
+
+		frame.pack();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+	}
+
+	private static void createLoginWindow()
+	{
+		// Creates a window with the title "Student Registration"
+		JFrame frame = new JFrame("Login");
+		// Sets it to terminate properly when closing the window
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		// Buttons for all options
 		JButton createAcct = new JButton("Create Account");
-		JButton studentLogin = new JButton("Student Login");
-		JButton teacherLogin = new JButton("Teacher Login");
-		JButton adminLogin = new JButton("Administration Login");
+		JButton login = new JButton("Log In");
 
 		// Actions for all buttons:
 		createAcct.setActionCommand(ACCT_CREATE_ACTION);
 		createAcct.addActionListener(new StudentRegistrationMain());
-		studentLogin.setActionCommand(STUDENT_LOGIN_ACTION);
-		studentLogin.addActionListener(new StudentRegistrationMain());
-		teacherLogin.setActionCommand(TEACHER_LOGIN_ACTION);
-		teacherLogin.addActionListener(new StudentRegistrationMain());
-		adminLogin.setActionCommand(ADMIN_LOGIN_ACTION);
-		adminLogin.addActionListener(new StudentRegistrationMain());
+		login.setActionCommand(LOGIN_ACTION);
+		login.addActionListener(new StudentRegistrationMain());
 
 		// Contents for inside our window, as well as the layout:
 		JPanel gui1 = new JPanel();
@@ -75,12 +115,11 @@ public class StudentRegistrationMain implements ActionListener
 
 		// Adds the buttons to our panel
 		gui1.add(createAcct);
-		gui1.add(studentLogin);
-		gui2.add(teacherLogin);
-		gui2.add(adminLogin);
+		gui1.add(login);
 
 		// Sets size and adds content to the window itself:
 		frame.setMinimumSize(new Dimension(372, 82));
+		frame.setMaximumSize(new Dimension(372, 82));
 		frame.getContentPane().add(gui1, BorderLayout.NORTH);
 		frame.getContentPane().add(gui2, BorderLayout.SOUTH);
 
@@ -97,43 +136,42 @@ public class StudentRegistrationMain implements ActionListener
 		JFrame frame = new JFrame("Account Creation");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+		JButton create = new JButton("Create Account");
 		JLabel usernameLabel = new JLabel("E-mail / Username:");
 		JLabel passwordLabel = new JLabel("Password:");
+
 		JTextField newUsername = new JTextField();
-		newUsername.setSize(40, 40);
-		JTextField newPassword = new JTextField();
-		newPassword.setSize(40, 40);
-		JButton create = new JButton("Create Account");
+		newUsername.setPreferredSize(new Dimension(212, 22));
+		JPasswordField newPassword = new JPasswordField();
+		newPassword.setPreferredSize(new Dimension(212, 22));
+		newPassword.setEchoChar('*');
 
-		JPanel guiUsername = new JPanel();
-		guiUsername.setLayout(new GridLayout());
-		JPanel guiPassword = new JPanel();
-		guiPassword.setLayout(new GridLayout());
+		create.setActionCommand(CONFIRM_ACCT_CREATION_ACTION);
+		create.addActionListener(new StudentRegistrationMain());
 
-		guiUsername.add(newUsername);
-		guiPassword.add(newPassword);
-		frame.setMinimumSize(new Dimension(256, 256));
-		frame.getContentPane().add(guiUsername, BorderLayout.NORTH);
-		frame.getContentPane().add(guiPassword, BorderLayout.SOUTH);
+		currentWIPPassword = new String(newPassword.getPassword());
+
+		JPanel gui = new JPanel(new MigLayout());
+
+		gui.add(usernameLabel);
+		gui.add(newUsername, "wrap");
+
+		gui.add(passwordLabel);
+		gui.add(newPassword, "wrap");
+
+		gui.add(create, "span");
+
+		frame.getContentPane().add(gui, BorderLayout.CENTER);
 
 		frame.pack();
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
 	}
 
-	private static void createStudentLoginWindow()
+	// TODO: create this window and add functionality
+	private static void createClassViewWindow()
 	{
-
-	}
-
-	private static void createTeacherLoginWindow()
-	{
-
-	}
-
-	private static void createAdminLoginWindow()
-	{
-
+		System.out.println("Not Yet Implemented");
 	}
 
 	@Override
@@ -143,12 +181,15 @@ public class StudentRegistrationMain implements ActionListener
 
 		if (ACCT_CREATE_ACTION.equals(cmd))
 			createAccountCreationWindow();
-		else if (STUDENT_LOGIN_ACTION.equals(cmd))
-			createStudentLoginWindow();
-		else if (TEACHER_LOGIN_ACTION.equals(cmd))
-			createTeacherLoginWindow();
-		else if (ADMIN_LOGIN_ACTION.equals(cmd))
-			createAdminLoginWindow();
+		else if (LOGIN_WINDOW_ACTION.equals(cmd))
+			createLoginWindow();
+		else if (VIEW_CLASSES_ACTION.equals(cmd))
+			createClassViewWindow();
+		else if (CONFIRM_ACCT_CREATION_ACTION.equals(cmd))
+		{
+			System.out.println("Button Hit");
+			passwords.add(currentWIPPassword);
+			System.out.println(passwords.get(0));
+		}
 	}
-
 }
