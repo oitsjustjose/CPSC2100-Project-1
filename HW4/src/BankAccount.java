9db63a041a1355cec14796543403e802684e2b88
oitsjustjose <stovallj1995@gmail.com>
2016-02-27 16:38:49 -0500

    Started and finished - easy peasy

--- HW4/src/BankAccount.java
+++ HW4/src/BankAccount.java
@@ -1,53 +1,58 @@
 /**
-   A bank account has a balance that can be changed by 
-   deposits and withdrawals.
-*/
+ * A bank account has a balance that can be changed by deposits and withdrawals.
+ */
 public class BankAccount
-{  
-   private double balance; 
+{
+	private double balance;
 
-   /**
-      Constructs a bank account with a zero balance.
-   */
-   public BankAccount()
-   {  
-      balance = 0;
-   }
+	/**
+	 * Constructs a bank account with a zero balance.
+	 */
+	public BankAccount()
+	{
+		balance = 0;
+	}
 
-   /**
-      Constructs a bank account with a given balance.
-      @param initialBalance the initial balance
-   */
-   public BankAccount(double initialBalance)
-   {  
-      balance = initialBalance;
-   }
- 
-   /** 
-      Deposits money into the account.
-      @param amount the amount of money to withdraw
-   */
-   public void deposit(double amount) 
-   {  
-      balance = balance + amount;
-   }
+	/**
+	 * Constructs a bank account with a given balance.
+	 * 
+	 * @param initialBalance
+	 *            the initial balance
+	 */
+	public BankAccount(double initialBalance)
+	{
+		balance = initialBalance;
+	}
 
-   /** 
-      Withdraws money from the account.
-      @param amount the amount of money to deposit
-   */
-   public void withdraw(double amount) 
-   {  
-      balance = balance - amount;
-   }
+	/**
+	 * Deposits money into the account.
+	 * 
+	 * @param amount
+	 *            the amount of money to withdraw
+	 */
+	public void deposit(double amount)
+	{
+		balance = balance + amount;
+	}
 
-   /** 
-      Gets the account balance.
-      @return the account balance
-   */
-   public double getBalance()
-   {  
-      return balance; 
-   }
+	/**
+	 * Withdraws money from the account.
+	 * 
+	 * @param amount
+	 *            the amount of money to deposit
+	 */
+	public void withdraw(double amount)
+	{
+		balance = balance - amount;
+	}
+
+	/**
+	 * Gets the account balance.
+	 * 
+	 * @return the account balance
+	 */
+	public double getBalance()
+	{
+		return balance;
+	}
 }
-
