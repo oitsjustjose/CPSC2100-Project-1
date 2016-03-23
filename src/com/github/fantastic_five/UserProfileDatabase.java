a0db11b89b99d2dac1f5a59d0671f34da0928018
unfallible <stephenclark314@gmail.com>
2016-03-23 16:33:58 -0400

    Updated UserProfileDatatbase and created Course class

--- src/com/github/fantastic_five/UserProfileDatabase.java
+++ src/com/github/fantastic_five/UserProfileDatabase.java
@@ -1,6 +1,5 @@
 package com.github.fantastic_five;
 
-import java.security.InvalidParameterException;
 import java.util.HashSet;
 
 /**
@@ -45,7 +44,9 @@ public class UserProfileDatabase
 
 	/**
 	 * Adds a new userProfile to the database iff !hasUser(user.getUserID)
-	 * @param user The userProfile being added
+	 * 
+	 * @param user
+	 *            The userProfile being added
 	 * @return Returns true iff hasUser(user.getUserID)
 	 */
 	public boolean addUserProfile(UserProfile user)
@@ -59,14 +60,29 @@ public class UserProfileDatabase
 		}
 		return hasUser;
 	}
-	
+
+	/**
+	 * Returns a userProfile with the specified userID and password, if such a userProfile exists in the database
+	 * @param userID The userID of the userProfile being returned
+	 * @param password The password of the userProfile being returned
+	 * @return The userProfile associated with the explicit parameters. Returns null if the userID does not exist or the password is incorrect.
+	 */
 	public UserProfile login(String userID, String password)
 	{
-		for(UserProfile e: users)
+		UserProfile user = null;
+		for (UserProfile e : users)
 		{
-			e.getUserID
+			if (e.getUserID().equals(userID))
+			{
+				if (e.passwordIs(password))
+				{
+					user = e;
+				}
+				break;
+			}
 		}
-		return null;
+		
+		return user;
 	}
-	
+
 }
