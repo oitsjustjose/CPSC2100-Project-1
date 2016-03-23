197d3a09a1033a286f80853d77bc1ed87f2d8413
unfallible <stephenclark314@gmail.com>
2016-03-23 17:10:30 -0400

    Dear God please let this work amen

--- src/com/github/fantastic_five/Logic/Course.java
+++ src/com/github/fantastic_five/Logic/Course.java
@@ -1,81 +1,328 @@
 package com.github.fantastic_five.Logic;
 
+import java.security.InvalidParameterException;
+import java.util.HashSet;
+
 public class Course
 {
-	private int CRN;
-	private String name;
-	private int section;
-	private String desc;
-	private String day;
-	private String time;
-	private int capacity;
+	private String title;
+	private String description;
+	private String crn;
+	private int studentCap;
+	private HashSet<Day> days;
+	private Time startTime;
+	private Time endTime;
+
+	public static final int TWENTYFOUR_HR_CLOCK = Time.TWENTYFOUR_HR_CLOCK;
+	public static final int TWELVE_HR_CLOCK = Time.TWELVE_HR_CLOCK;
+	
+	/**
+	 * Constructs a new course object
+	 * @param title The title of the course
+	 * @param description A description of the course
+	 * @param crn The course's CRN number
+	 * @param studentCap The maximum number of students that may enroll in the course;
+	 * @param days The days that the course meets on
+	 * @param startTimeHr A whole number less than 24, representing the hour of the day at which the course meets
+	 * @param startTimeMin A whole number less than 60, representing the minute of the hour at which the course meets
+	 * @param endTimeHr A whole number less than 24, representing the hour of the day at which the course ends
+	 * @param endTimeMin A whole number less than 60, representing the minute of the hour at which the course ends
+	 */
+	public Course(String title, String description, String crn, int studentCap, HashSet<Day> days,
+			int startTimeHr, int startTimeMin, int endTimeHr, int endTimeMin)
+	{
+		this.title = title;
+		this.description = description;
+		this.crn = crn;
+		this.studentCap = studentCap;
+		this.days = days;
+		startTime = new Time(startTimeHr, startTimeMin);
+		endTime = new Time(endTimeHr, endTimeMin);
+		checkTimes();
+	}
 
 	/**
+	 * Returns the title of the course
 	 * 
-	 * @param CRN
-	 *            Unique CRN number
-	 * @param name
-	 *            Name of the course
-	 * @param section
-	 *            The Section number of the course
-	 * @param description
-	 *            Like "English I", or "Intro to OS"
-	 * @param day
-	 *            Convention: "MTWTrF"
-	 * @param time
-	 *            Military Time; Convention: "09:00-13:00"
-	 * @param capacity
-	 *            Max students that can be in the class
+	 * @return The title of the course
 	 */
-	public Course(int CRN, String name, int section, String description, String day, String time, int capacity)
+	public String getTitle()
 	{
-		this.CRN = CRN;
-		this.name = name;
-		this.section = section;
-		this.desc = description;
-		this.day = day;
-		this.time = time;
-		this.capacity = capacity;
+		return title;
 	}
 
-	public int getCRN()
+	/**
+	 * Returns the description of the course
+	 * 
+	 * @return The description of the course
+	 */
+	public String getDescription()
 	{
-		return this.CRN;
+		return description;
+	}
+	
+	public void setDescription(String newDescription)
+	{
+		description = newDescription;
 	}
 
-	public String getName()
+	/**
+	 * Returns the crn of the course
+	 * 
+	 * @return The crn of the course
+	 */
+	public String getCRN()
 	{
-		return this.name;
+		return crn;
 	}
 
-	public int getSection()
+	/**
+	 * Returns the maximum number of student that can be enrolled in the course
+	 * 
+	 * @return The maximum number of student that can be enrolled in the course
+	 */
+	public int getStudentCap()
 	{
-		return this.section;
+		return studentCap;
 	}
 
-	public String getDesc()
+	/**
+	 * Returns the meeting days for the course
+	 * 
+	 * @return The meeting days for the course
+	 */
+	public HashSet<Day> getDays()
 	{
-		return this.desc;
+		return days;
 	}
 
-	public String getDays()
+	/**
+	 * Returns a string representing the start time for the course, in either the 24 hour notation or the 12 hour notation
+	 * @param clockType Must be either TWENTYFOUR_HOUR_CLOCK or TWELVE_HOUR_CLOCK
+	 * @return A string representing the start time for the course
+	 */
+	public String getStartTime(int clockType)
 	{
-		return this.day;
+		String rVal = startTime.formatTime(clockType);
+		return rVal;
 	}
 
-	public String getTimes()
+	/**
+	 * Returns a string representing the end time for the course, in either the 24 hour notation or the 12 hour notation
+	 * @param clockType Must be either TWENTYFOUR_HOUR_CLOCK or TWELVE_HOUR_CLOCK
+	 * @return A string representing the end time for the course
+	 */
+	public String getEndTime(int clockType)
 	{
-		return this.time;
+		String rVal = endTime.formatTime(clockType);
+		return rVal;
+	}
+	
+	/**
+	 * Tests to see if this course and the specified course meet at conflicting times
+	 * @param other The other course
+	 * @return Returns true iff the courses conflict
+	 */
+	public boolean conflictsWith(Course other)
+	{
+		boolean coursesConflict;
+		
+		HashSet<Day> intersection = (HashSet<Day>) days.clone();
+		intersection.retainAll(other.days);
+		
+		//Checks if the courses ever meet on the same day
+		if(intersection.size() > 0)
+		{
+			//Checks if this course ends before the other starts
+			if(this.endTime.compareTo(other.startTime)< 0)
+			{
+				coursesConflict = false;
+			}
+			else
+			{
+				//checks if this course starts after the other ends
+				if(this.startTime.compareTo(other.endTime) > 0)
+				{
+					coursesConflict = false;
+				}
+				else
+				{
+					coursesConflict = true;
+				}
+			}
+		}
+		else
+		{
+			coursesConflict = false;
+		}
+		
+		return coursesConflict;
+	}
+	
+	/**
+	 * Checks to see if the course's endTime is later than its startTime.
+	 * If it isn't, throws an IllegalStateException.
+	 */
+	private void checkTimes()
+	{
+		if(startTime.compareTo(endTime) >= 0)
+		{
+			throw new IllegalStateException("Course must start before it can end");
+		}
+			
+	}
+	
+	/**
+	 * Represents a time of day, down to the minute. 
+	 * 
+	 * @author Fantastic Five
+	 *
+	 */
+	private class Time implements Comparable<Time>
+	{	
+		private int hr;
+		private int min;
+		
+		public static final int TWENTYFOUR_HR_CLOCK = 24;
+		public static final int TWELVE_HR_CLOCK = 12;
+		
+		/**
+		 * 
+		 * @param hr A whole number less than 24, representing the hour of the day
+		 * @param min A whole number less than 60, representing the minute of the hour
+		 */
+		public Time(int hr, int min)
+		{
+			if(hr >= 23 || hr < 0)
+			{
+				throw new InvalidParameterException("hr is invalid");
+			}
+			
+			if(min >= 60 || min < 0)
+			{
+				throw new InvalidParameterException("min is invalid");
+			}
+			
+			this.hr = hr;
+			this.min = min;
+		}
+		
+		public String formatTime(int clockType)
+		{
+			String rVal;
+			String fHour;
+			String fMinute;
+			String fPeriod;
+
+			final int HALF_DAY = 12;
+			final String AM = " a.m.";
+			final String PM = " p.m.";
+
+			if (clockType == TWELVE_HR_CLOCK)
+			{
+				if ((hr % HALF_DAY) == 0)
+				{
+					fHour = String.valueOf(HALF_DAY);
+
+					if ((hr / HALF_DAY) == 0)
+					{
+						fPeriod = PM;
+					}
+					else
+					{
+						fPeriod = AM;
+					}
+				}
+				else
+				{
+					fHour = String.valueOf(hr % HALF_DAY);
+
+					if ((hr / HALF_DAY) == 0)
+					{
+						fPeriod = AM;
+					}
+					else
+					{
+						fPeriod = PM;
+					}
+				}
+			}
+			else if(clockType == TWENTYFOUR_HR_CLOCK)
+			{
+				fHour = String.valueOf(hr);
+				fPeriod = "";
+			}
+			else
+				{
+					throw new InvalidParameterException("Invalid clockType");
+				}
+			
+			if (min < 10)
+			{
+				fMinute = "0" + min;
+			}
+			else
+			{
+				fMinute = String.valueOf(min);
+			}
+			
+			rVal = fHour+":"+fMinute+fPeriod;
+
+			return rVal;
+		}
+
+		public int compareTo(Time other)
+		{
+			int rVal;
+			if(this.hr < other.hr)
+			{
+				rVal = -1;
+			}
+			else if(this.hr == other.hr)
+			{
+				if(this.min < other.min)
+				{
+					rVal = -1;
+				}
+				else if(this.min == other.min)
+				{
+					rVal = 0;
+				}
+				else
+				{
+					rVal = 1;
+				}
+			}
+			else
+			{
+				rVal = 1;
+			}
+			
+			return rVal;
+		}
 	}
 
-	public int getCap()
+	public static enum Day
 	{
-		return this.capacity;
-	}
+		MONDAY("Monday", "M"), TUESDAY("Tuesday", "T"), WEDNESDAY("Wednesday", "W"), THURSDAY("Thursday", "R"), FRIDAY("Friday", "F"), SATURDAY("Saturday", "S"), SUNDAY("Sunday", "U");
 
-	@Override
-	public String toString()
-	{
-		return CRN + " | " + name + " | " + section + " | " + desc + " | " + day + " | " + time + " | " + capacity;
+		private Day(String name, String abbreviation)
+		{
+			this.name = name;
+			this.abbreviation = abbreviation;
+		}
+
+		private String name;
+		private String abbreviation;
+
+		public String getName()
+		{
+			return name;
+		}
+
+		public String getAbbreviation()
+		{
+			return abbreviation;
+		}
 	}
 }
